4f7473cf9bd31f457849e474f623a38fa8551c61
OrusLiOx <y.lily.n@gmail.com>
2023-04-04 15:06:21 -0500

    modified:   Matrix.java 	modified:   Main.java

--- Matrix.java
+++ Matrix.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Matrix 
 {
     private ArrayList<ArrayList<Integer>> matrix;
+    int mostDigits=1;
 
     public Matrix()
     {
@@ -13,24 +14,56 @@ public class Matrix
     public int get(int x,int y) { return matrix.get(y).get(x); }
     public boolean isEmpty()    { return matrix.isEmpty(); }
 
+    // adds row to the bottom of matrix
     public void addRow(String row)
     {
         matrix.add(new ArrayList<Integer>());
         Scanner line = new Scanner(row);
         while(line.hasNext())
         {
-            matrix.get(getHeight()-1).add(line.nextInt());
+            int num =line.nextInt();
+            if(Math.pow(10,mostDigits) < num)
+                mostDigits = (int)Math.log10(num)+1;
+            matrix.get(getHeight()-1).add(num);
         }
+        line.close();
     }
     public void print()
     {
-        for(ArrayList<Integer> line : matrix )
+        for(int i = 0; i<getHeight();i++)
         {
-            for(Integer i : line)
-            {
-                System.out.print(i+ " ");
-            }
+            printRow(i);
             System.out.println();
         }
+    }
+    public void printRow(int r)
+    {
+        if(r >= getHeight())
+        {
+            String spaces = "";
+            //System.out.println(4 + " " + getWidth() + " " + (mostDigits+1));
+            for(int k = 3+getWidth()*(mostDigits+1); k>0; k--)
+            {
+                spaces += " ";
+            }
+            System.out.print(spaces);
+        }
+        else
+        {
+            System.out.print("| ");
+            for(Integer i : matrix.get(r))
+            {
+                System.out.print(normalizedNumber(i)+ " ");
+            }
+            System.out.print("|");
+        }
+    }
+
+    private String normalizedNumber(int num)
+    {
+        String s = ""+num;
+        for(int dig = (int)Math.log10(num)+1; dig < mostDigits; dig++)
+            s = " " + s;
+        return s;
     }
 }
