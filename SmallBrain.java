ee6ab70ec3910c21198e055588521af20601418c
Wyatt5150 <wyatt.mahoney5150@gmail.com>
2023-04-19 13:34:50 -0500

    s

--- SmallBrain.java
+++ SmallBrain.java
@@ -1,7 +1,7 @@
 public class SmallBrain extends MatrixMultiply
 {
 
-    public static Matrix multiply(Matrix m1, Matrix m2)
+    public Matrix multiply(Matrix m1, Matrix m2)
     {
         if(!isValid(m1, m2))
         {
