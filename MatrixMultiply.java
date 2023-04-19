fe6a088afff6c00e3213434a35ad5d36f9b82802
OrusLiOx <y.lily.n@gmail.com>
2023-04-19 13:02:38 -0500

    modified:   MatrixMultiply.java

--- MatrixMultiply.java
+++ MatrixMultiply.java
@@ -1,10 +1,10 @@
-public class MatrixMultiply 
+public abstract class MatrixMultiply 
 {
     // revieve 2 matrices, return product
-    //abstract public Matrix multiply(Matrix m1, Matrix m2);
+    abstract public Matrix multiply(Matrix m1, Matrix m2);
     
     // revieve 2 matrices and which row of m1 and which col of m2 is getting calculated
-    public int calc(Matrix m1, Matrix m2, int r, int c)
+    public static int calc(Matrix m1, Matrix m2, int r, int c)
     {
         int sol = 0;
         for(int k = 0; k < m1.getWidth (); k++)
@@ -16,7 +16,7 @@ public class MatrixMultiply
     }
 
     // return if m1 and m2 can be multipled together
-    public boolean isValid(Matrix m1, Matrix m2)
+    public static boolean isValid(Matrix m1, Matrix m2)
     {
         return m1.getWidth()==m2.getHeight();
     }
