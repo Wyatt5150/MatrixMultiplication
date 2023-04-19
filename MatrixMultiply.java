61002a125e0e03f7c312251ff1bd570cb28377ca
Wyatt5150 <wyatt.mahoney5150@gmail.com>
2023-04-19 13:26:17 -0500

    s

--- MatrixMultiply.java
+++ MatrixMultiply.java
@@ -4,7 +4,7 @@ public class MatrixMultiply
     //abstract public Matrix multiply(Matrix m1, Matrix m2);
     
     // revieve 2 matrices and which row of m1 and which col of m2 is getting calculated
-    public int calc(Matrix m1, Matrix m2, int r, int c)
+    public static int calc(Matrix m1, Matrix m2, int r, int c)
     {
         int sol = 0;
         for(int k = 0; k < m1.getWidth (); k++)
@@ -16,8 +16,16 @@ public class MatrixMultiply
     }
 
     // return if m1 and m2 can be multipled together
-    public boolean isValid(Matrix m1, Matrix m2)
+    public static boolean isValid(Matrix m1, Matrix m2)
     {
         return m1.getWidth()==m2.getHeight();
+    }
+
+    public static void invalidSize(Matrix m1, Matrix m2)
+    {
+        System.out.println("Invalid Matrix Sizes.");
+        System.out.println("Matrix 1 Width Must Match Matrix 2 Height.");
+        System.out.println("Matrix 1 Width: " + m1.getWidth());
+        System.out.println("Matrix 2 Height: " + m2.getHeight());
     }
 }
