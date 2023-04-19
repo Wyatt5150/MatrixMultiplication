8b359c8e9c767524619f851039b0b9ceed77ddea
Wyatt5150 <wyatt.mahoney5150@gmail.com>
2023-04-19 12:34:08 -0500

    Merge branch 'main' of https://github.com/Wyatt5150/MatrixMultiplication

--- RunTimer.java
+++ RunTimer.java
@@ -4,26 +4,18 @@ class RunTimer
     private long endTime;
 
     public void start()
-    public RunTimer()
-    {
-
-    }
-
-    public void start()
     {
         startTime = System.nanoTime();
         endTime = System.nanoTime();
     }
 
     public void stop()
-    public void stop()
     {
         endTime = System.nanoTime();
         display();
         display();
     }
 
-    private void display()
     private void display()
     {
         System.out.println((endTime - startTime) / (double)1000000000);
